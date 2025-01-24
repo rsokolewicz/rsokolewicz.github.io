615ada203e795e7ae3d88dd78bbf5e128348fb00
Robert Sokolewicz <rsokolewicz@gmail.com>
2025-01-24 20:47:30 +0100

    rebuilding site Fri Jan 24 20:47:26 CET 2025

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,17 @@ fi
 git pull
 git add .
 git reset -- public/*
+git submodule update --remote --merge themes/hugo-theme-terminal/
+
+# Check if there are changes in the submodule
+cd themes/hugo-theme-terminal/
+if [ -n "$(git status --porcelain)" ]; then
+    git add .
+    git commit -m "Update submodule: $msg"
+    git push
+fi
+cd ../..
+
+git add themes/hugo-theme-terminal/
 git commit -m "$msg"
 git push origin main
